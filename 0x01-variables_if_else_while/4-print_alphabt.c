f34bc470fd5113c50bd9c1d0f36c4698908bf198
LadyNaa <marthalady13@gmail.com>
2022-12-11 22:44:03 -0800

    alha in lc

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z' lc++)
+	{
+		if (lc != 'e' && lc != 'q')
+			putchar(lc);
+	}
+
+	putchar('\n');
+	return (0);
+}
