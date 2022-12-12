073da849b84c14eda038440fa950c13df109fb1a
LadyNaa <marthalady13@gmail.com>
2022-12-12 04:30:41 -0800

    alphabet except q,e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,13 +2,14 @@
 
 /**
  * main - alphabet in lowercase
+ * except q and e
  * Return: Always 0
  */
 int main(void)
 {
 	char lc;
 
-	while (lc = 'a'; lc <= 'z' lc++)
+	for (lc = 'a'; lc <= 'z'; lc++)
 	{
 		if (lc != 'e' && lc != 'q')
 			putchar(lc);
