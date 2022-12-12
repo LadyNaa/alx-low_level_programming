eac3b05ea759c8bd40fd123be28cc9f5a2240270
LadyNaa <marthalady13@gmail.com>
2022-12-12 05:38:28 -0800

    combi-single digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@ int main(void)
 {
 	int digit;
 
-	for (digit = 48; digit < 10; digit++)
+	for (digit = 48; digit < 58; digit++)
 	{
 		putchar(digit);
-		if (digit != 9)
+		if (digit != 57)
 		{
 			putchar(',');
 			putchar(' ');
@@ -20,5 +20,5 @@ int main(void)
 
 		putchar('\n');
 		return (0);
-	}
+
 }
