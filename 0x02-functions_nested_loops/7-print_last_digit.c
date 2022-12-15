b373e47cc53c8825d4ba1c3eefb8f0c30e568a17
LadyNaa <marthalady13@gmail.com>
2022-12-14 19:14:17 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: num
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int la_d = n % 10;
+
+	if (la_d < 0)
+		la_d *= -1;
+
+	_putchar(la_d + '0');
+
+	return (0);
+}
