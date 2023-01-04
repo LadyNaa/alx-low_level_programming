828ceaec985f2cc87efd7556f9459deb16308039
LadyNaa <marthalady13@gmail.com>
2023-01-03 18:28:16 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory
+ * @dest: stored memory
+ * @src: copied memory
+ * @n: num of byte
+ *
+ * Return: copied memory
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
