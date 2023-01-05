13adafc921e65121b896365990990f1dc1481d72
LadyNaa <marthalady13@gmail.com>
2023-01-04 20:26:30 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character  in a string
- * @s: string searched
- * @c: xter tobe located
- *
- * Retutn: If c is found - a pointer to the 1st occurrence
- * If is not found - NULL
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Retutn: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
-			return (s + i);
+			return (&s[i]);
 	}
-	return ('\0');
+	return (0);
 }
